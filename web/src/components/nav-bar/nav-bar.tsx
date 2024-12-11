90cc77bfce21c757bf812ebcff2cbeb5d6dd1a32
isabe <isabellerougem@gmail.com>
2024-12-11 13:26:18 -0600

    chore: fix look of navbar

--- web/src/components/nav-bar/nav-bar.tsx
+++ web/src/components/nav-bar/nav-bar.tsx
@@ -9,35 +9,15 @@ import { Button, Link } from "@nextui-org/react";
 
   export default function Nav() {
     return (
-      
-      <Navbar position="static" isBordered>
+      <Navbar position="static" isBordered className="bg-[#D4D4D4]">
         <NavbarBrand>
-          <p className="font-bold text-inherit">ACME</p>
+          <p className="font-bold text-inherit text-slate-900">STARR Lab Rad-Effects Database</p>
         </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-4" justify="center">
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Features
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link href="#" aria-current="page">
-              Customers
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              Integrations
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
         <NavbarContent justify="end">
-          <NavbarItem className="hidden lg:flex">
-            <Link href="#">Login</Link>
-          </NavbarItem>
+          {/* TODO: add which nsid is logged in */}
           <NavbarItem>
-            <Button as={Link} color="primary" href="#" variant="flat">
-              Sign Up
+            <Button className="bg-usask-green" variant="flat">
+              <span className="text-slate-50">Log out</span>
             </Button>
           </NavbarItem>
         </NavbarContent>
