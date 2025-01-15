7e09c9e87493149bb777d68974491ce05215bb76
ConnerWithAnE <conner.hnatiuk@usask.ca>
2025-01-15 14:24:55 -0600

    Simple implementation of FrontPage

--- web/src/pages/FrontPage.tsx
+++ web/src/pages/FrontPage.tsx
@@ -1,40 +1,49 @@
-import { useNavigate } from "react-router-dom";
-import "../App.css";
+import { useNavigate } from 'react-router-dom';
+import '../App.css';
+import { Button } from '@nextui-org/react';
 
 function redirectToCAS() {
-    const CAS_SERVER = "https://cas.usask.ca/cas";
-    const FRONTEND_URL = "https://starr-lab-server.usask.ca";
-    const serviceURL = `${FRONTEND_URL}/cas-callback`;
+  const CAS_SERVER = 'https://cas.usask.ca/cas';
+  const FRONTEND_URL = 'https://starr-lab-server.usask.ca';
+  const serviceURL = `${FRONTEND_URL}/cas-callback`;
 
-    window.location.href = `${CAS_SERVER}/login?service=${encodeURIComponent(
-        serviceURL
-    )}`;
+  window.location.href = `${CAS_SERVER}/login?service=${encodeURIComponent(
+    serviceURL
+  )}`;
 }
 
-
 export default function FrontPage() {
-    const navigate = useNavigate();
-    const token = localStorage.getItem("jwt");
+  const navigate = useNavigate();
+  const token = localStorage.getItem('jwt');
 
-    if (token) {
-        navigate("/modify");
-        return null; // Optional: Display a loading spinner while redirecting
-    } 
+  if (token) {
+    navigate('/modify');
+    return null; // Optional: Display a loading spinner while redirecting
+  }
 
-    const loginClicked = () => {
-        redirectToCAS();
-        navigate("/modify");
-    }
+  const loginClicked = () => {
+    redirectToCAS();
+  };
 
-    return (
-        <div>
-            <div>
-                <h1>STARR Lab Rad-Effects Database</h1>
-                <p>Login</p>
-                <button onClick={loginClicked}>Usask Login</button>
-                <p>Unable to login? Contact the system administrator</p>
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex items-center justify-center p-[10%]">
+      <div className="p-8 border border-gray-300 rounded-lg shadow-lg text-center">
+        <h1 className="text-2xl font-semibold mb-4 text-wrap">
+          STARR Lab <br />
+          Rad-Effects Database
+        </h1>
+        <h1 className="mb-4 py-10 pt-14 text-2xl">Login</h1>
+        <Button
+          className="bg-usask-green text-white text-2xl px-12 py-6 rounded-lg "
+          onClick={loginClicked}
+        >
+          Usask Login
+        </Button>
 
+        <p className="pt-[40%] text-sm text-gray-500">
+          Unable to login? Contact the system administrator
+        </p>
+      </div>
+    </div>
+  );
 }
