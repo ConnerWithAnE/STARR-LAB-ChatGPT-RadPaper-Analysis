ba6d37d5968dd98eff77c516d3ac1e6b0ce92201
ConnerWithAnE <conner.hnatiuk@usask.ca>
2025-03-09 12:37:31 -0600

    Updated the admin and cascade router, remove unneeded files

--- server/src/routes/admin-router.ts
+++ server/src/routes/admin-router.ts
@@ -1,12 +1,19 @@
 import express, { Request, Response, Router, NextFunction } from "express";
 import { DatabaseController } from "../database-controller";
-import { GetQuery, GPTResponse, Testing } from "../types";
+import {
+  ai_FullDataType,
+  ai_GPTResponse,
+  GetQuery,
+  GPTResponse,
+  Testing,
+} from "../types";
 import axios from "axios";
 import jwt from "jsonwebtoken";
 import authenticateJWT from "../auth/jwt-auth";
 import { GPTController } from "../gpt-controller";
 import multer from "multer";
 import config from "../config";
+import { GenericController } from "../generic-controller";
 
 // Set up custom storage for multer
 const storage = multer.diskStorage({
@@ -34,7 +41,7 @@ function getAuthMiddleware() {
 }
 
 export default function adminRouter(
-  dbController: DatabaseController,
+  dbController: GenericController,
   gptController: GPTController,
 ): Router {
   const router = Router();
@@ -43,7 +50,7 @@ export default function adminRouter(
   // The data is in the correct format and ready to be input.
 
   // THIS WILL NOT WORK WITH RAW PAPERS, Data MUST be in InsertData format
-  
+  /*
   router.post(
     "/insertPapers",
     getAuthMiddleware(),
@@ -75,6 +82,7 @@ export default function adminRouter(
       }
     }
   );
+  */
 
   router.post(
     "/parseRequest",
@@ -90,7 +98,7 @@ export default function adminRouter(
           });
         } else {
           await parsePapers(req.files, gptController).then(
-            (result: GPTResponse[]) => {
+            (result: ai_GPTResponse[]) => {
               console.log(responseToJSON(result));
               res.send(responseToJSON(result));
             },
@@ -101,6 +109,7 @@ export default function adminRouter(
       }
     },
   );
+  /*
 
   router.post(
     "/getFullPapers",
@@ -118,6 +127,259 @@ export default function adminRouter(
       }
     },
   );
+  */
+
+  /** Create a new record for any model */
+  router.post("/:model", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const modelName = req.params.model;
+      const data = req.body;
+      delete data.id;
+
+      const createdInstance = await GenericController.create(modelName, data);
+
+      if (!Array.isArray(createdInstance) && "error" in createdInstance) {
+        res
+          .status(createdInstance.status || 400)
+          .json({ error: createdInstance.error });
+        return;
+      }
+
+      res.status(201).json(createdInstance);
+    } catch (error) {
+      console.error(`Error creating ${req.params.model}:`, error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.post("/papers/full", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const data = req.body;
+      console.log("Received request for full paper creation:", data);
+
+      const createdPaper = await GenericController.createFullPaper(data);
+
+      if (createdPaper.error) {
+        res
+          .status(createdPaper.status || 500)
+          .json({ error: createdPaper.error });
+      }
+
+      res.status(201).json(createdPaper);
+    } catch (error) {
+      console.error("Error creating full paper:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.put("/papers/full/:id", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const id = parseInt(req.params.id, 10);
+      if (isNaN(id)) {
+        res.status(400).json({ error: "Invalid ID format" });
+        return;
+      }
+
+      const data = req.body;
+      const append = data.append;
+      delete data.append; // Remove append flag from request body
+
+      console.log(
+        `Received request to update paper ID ${id} with append=${append}`,
+      );
+      const updatedPaper = await GenericController.updateFullPaper(
+        id,
+        data,
+        append,
+      );
+
+      if (updatedPaper.error) {
+        res
+          .status(updatedPaper.status || 500)
+          .json({ error: updatedPaper.error });
+      } else {
+        res.json(updatedPaper);
+      }
+    } catch (error) {
+      console.error("Error updating full paper:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.get("/papers/full", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      console.log("Received request to fetch all full papers.");
+      const papers = await GenericController.getFullPaper();
+
+      res.json(papers);
+    } catch (error) {
+      console.error("Error fetching all full papers:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.get("/papers/full/:id", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const id = parseInt(req.params.id, 10);
+      if (isNaN(id)) {
+        res.status(400).json({ error: "Invalid paper ID format" });
+        return;
+      }
+
+      console.log(`Received request to fetch full paper with ID: ${id}`);
+      const paper = await GenericController.getFullPaperById(id);
+
+      if (paper.error) {
+        res.status(paper.status || 500).json({ error: paper.error });
+        return;
+      }
+
+      res.json(paper);
+    } catch (error) {
+      console.error("Error fetching full paper:", error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  /** Get all records for any model */
+  router.get("/:model", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const modelName = req.params.model;
+      console.log(`Processing GET request for model: ${modelName}`);
+
+      const records = await GenericController.getAll(modelName);
+
+      if (!Array.isArray(records) && "error" in records) {
+        res.status(records.status || 500).json({ error: records.error });
+        return;
+      }
+
+      res.json(records);
+    } catch (error) {
+      console.error(`Error retrieving ${req.params.model}:`, error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  router.get("/:model/filter", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const modelName = req.params.model;
+      const filters = req.query; // Extract query parameters
+
+      console.log(`Filtering ${modelName} with`, filters);
+
+      const records = await GenericController.filter(modelName, filters);
+
+      if (!Array.isArray(records) && "error" in records) {
+        res.status(records.status || 500).json({ error: records.error });
+        return;
+      }
+
+      res.json(records);
+    } catch (error) {
+      console.error(`Error filtering ${req.params.model}:`, error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+  /**  Route to get a single record by ID */
+  router.get("/:model/:id", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const modelName = req.params.model;
+      const id = parseInt(req.params.id, 10);
+
+      if (isNaN(id)) {
+        res.status(400).json({ error: "Invalid ID format" });
+        return;
+      }
+
+      const record = await GenericController.getById(modelName, id);
+
+      if (!record) {
+        res.status(404).json({ error: "Record not found" });
+        return;
+      }
+
+      if (record.error) {
+        res.status(record.status || 500).json({ error: record.error });
+        return;
+      }
+
+      res.json(record);
+    } catch (error) {
+      console.error(
+        `Error retrieving ${req.params.model} with ID ${req.params.id}:`,
+        error,
+      );
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  /**  Update a record by ID for any model */
+  router.put("/:model/:id", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const modelName = req.params.model;
+      const id = parseInt(req.params.id, 10);
+
+      if (isNaN(id)) {
+        res.status(400).json({ error: "Invalid ID format" });
+        return;
+      }
+
+      const data = req.body;
+      const append = data.append || false; // Default is `false` (replace mode)
+      delete data.append; // Remove `append` flag from request body
+      delete data.id;
+
+      const updatedInstance = await GenericController.update(
+        modelName,
+        id,
+        data,
+        append,
+      );
+
+      if (updatedInstance.error) {
+        res
+          .status(updatedInstance.status || 500)
+          .json({ error: updatedInstance.error });
+        return;
+      }
+
+      res.json(updatedInstance);
+    } catch (error) {
+      console.error(`Error updating ${req.params.model}:`, error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
+
+  /**  Delete a record by ID for any model */
+  router.delete("/:model/:id", getAuthMiddleware(), async (req: Request, res: Response) => {
+    try {
+      const modelName = req.params.model;
+      const id = parseInt(req.params.id, 10);
+
+      if (isNaN(id)) {
+        res.status(400).json({ error: "Invalid ID format" });
+        return;
+      }
+
+      const deletedCount = await GenericController.delete(modelName, id);
+
+      if (deletedCount === 0) {
+        res
+          .status(404)
+          .json({ error: `No record found with ID ${id} in ${modelName}` });
+        return;
+      } else if (deletedCount === -1) {
+        res.status(400).json({ error: `Invalid model name: ${modelName}` });
+        return;
+      }
+
+      res.json({ message: `${modelName} deleted successfully` });
+    } catch (error) {
+      console.error(`Error deleting ${req.params.model}:`, error);
+      res.status(500).json({ error: "Internal Server Error" });
+    }
+  });
 
   // Example list of allowed NSIDs (replace with database table in the future)
   const allowedNSIDs = [
@@ -172,6 +434,7 @@ export default function adminRouter(
   return router;
 }
 
+/*
 async function insertRows(
   insertData: InsertData[],
   dbcontroller: DatabaseController,
@@ -180,27 +443,30 @@ async function insertRows(
     await dbcontroller.insertPaper(insertData[paper]);
   }
 }
+  */
 
 async function parsePapers(
   files: any,
   gptController: GPTController,
-): Promise<GPTResponse[]> {
+): Promise<any[]> {
   const fileList: string[] = files.map(
     (file: Express.Multer.File) => file.path,
   );
   console.log(fileList);
-  const gptResults = await gptController.runGPTAnalysis(fileList);
+  const gptResults = await gptController.processRadiationPapers(fileList);
   return gptResults;
 }
 
+/*
 async function getFullPaperRows(
   search: string,
   dbController: DatabaseController,
 ): Promise<TableData[]> {
   return await dbController.getFullData(search);
 }
+  */
 
-function insertDataRequestFromJSON(body: any): InsertData[] {
+function insertDataRequestFromJSON(body: any): ai_FullDataType[] {
   // Ensure body is an array of objects matching the InsertData structure
   if (!Array.isArray(body)) {
     throw new Error("Invalid body format: expected an array.");
@@ -209,10 +475,10 @@ function insertDataRequestFromJSON(body: any): InsertData[] {
   // Return a list of rows to insert
   return body.map((entry) => {
     // Return the validated entry as InsertData
-    return { ...entry } as InsertData;
+    return { ...entry } as ai_FullDataType;
   });
 }
 
-function responseToJSON(radDataArray: GPTResponse[]): string {
+function responseToJSON(radDataArray: ai_GPTResponse[]): string {
   return JSON.stringify(radDataArray, null, 2); // null and 2 prettify the JSON
 }
