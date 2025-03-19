2a8e13379a58c368a4281e77191ca8dcc4fdb1ed
ConnerWithAnE <conner.hnatiuk@usask.ca>
2025-03-19 09:30:08 -0600

    Fixed prompts and datatypes, added a new command line argument for running tables.

--- server/src/config.ts
+++ server/src/config.ts
@@ -1,6 +1,7 @@
 const config = {
   MockData: process.env.MOCK_DATA === 'true',
-  AuthEnable: process.env.AUTH_OFF === 'false'
+  AuthEnable: process.env.AUTH_OFF === 'false',
+  RunTables: process.env.GPT_RUNTABLES === 'true'
 };
 
 export default config;
